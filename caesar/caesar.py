e71b194c39cf9ab31e547df0965a6d211cc7b2e0
Greg Smith <gregsmith446@gmail.com>
2018-09-08 14:51:03 +0000

    working version, made adjustments to for loop

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -1,3 +1,4 @@
+# run program by typing 'python caesar.py' and an int that will serve as a key
 # program will prompt for a message to be encrypted (plaintext)
 # and outputs an encrypted version of that message (cipertext)
 # the plaintext is shifted based off the numerical key given in the command line
@@ -10,38 +11,31 @@ def main():
     if len(sys.argv) != 2:
         print("Provide a key after running the program")
         exit(1)
-# 1 get the key
-# cast key to integer
+# 1 get the key, cast key to integer
     key = int(sys.argv[1])
 # 2 get the plaintext
-    ciphertext = []
     plaintext = cs50.get_string("Message to Encrypt: ")
-    # print("Starting message is:", plaintext)
+    # initialize variable to hold ciphertext
+    ciphertext = []
+    print("ciphertext: ", end="")
 
-# checking characters
-# preserve case
-    for letter in plaintext:
-        if letter.isalpha():
-            ciphertext.append(caesar(char, key))
-        else:
-            ciphertext.append(letter)
-
-        print("".join(ciphertext))
-        exit(0)
-
-# 3 encypher text
-#ASCII <--> alphabetical
-# ord, chr
-# shift char by key
-# 4 print ciphertext
-# print()
-
-    def caesar(char, key):
-        if char.isupper():
-            return chr(((ord(char) - 65 + key) % 26) + 65)
-        else:
-            return chr(((ord(char) - 97 + key) % 26) + 97)
-
+# check characters for alpha + case, preserving case
+# 3 once checked, encypher the text
+    for x in range(len(plaintext)): # loop through plaintext string
+        if plaintext[x].isalpha(): # check char is alpha
+            if plaintext[x].isupper(): # check char is uppercase
+                charIndex = ord(plaintext[x]) - 65 # set uppercase A-Z index to 0, by subtract 65
+                ciphertext = chr((charIndex + key) % 26 + 65) # shift the char by they key, re-adding 65
+                print(ciphertext, end="") # print shifted uppercase chars
+            elif plaintext[x].islower(): # if lowercase
+                charIndex = ord(plaintext[x]) - 97 # set lowercase a-z index to 0, by subtracting 97
+                ciphertext = chr((charIndex + key) % 26 + 97) # shift the char by the key, re-adding 97
+                print(ciphertext, end="") # print shifted lowercase chars
+        else: # if not alpha, rpint as is
+            ciphertext = plaintext[x]
+            print(ciphertext, end="")
+    # 4 print completed ciphertext on a new line
+    print()
 
 if __name__ == "__main__":
     main()
